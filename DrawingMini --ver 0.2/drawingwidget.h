7cc6373fdeb4464684fbcdaef605360ad2802a62
propellanesjc <2769649101@qq.com>
2025-05-02 19:03:24 +0800

    sjc ver0.2

---
+++ DrawingMini --ver 0.2/drawingwidget.h
@@ -0,0 +1,42 @@
+#ifndef DRAWINGWIDGET_H
+#define DRAWINGWIDGET_H
+
+#include "drawingtools.h"
+#include <QWidget>
+#include <QPainter>
+#include <QMouseEvent>
+
+namespace Ui {
+class DrawingWidget;
+}
+
+class DrawingWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit DrawingWidget(QWidget *parent = nullptr);
+    ~DrawingWidget();
+    // 画笔
+    DrawingTools *pen;
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    Ui::DrawingWidget *ui;
+    // 是否画画
+    bool drawing;
+    // 记录鼠标坐标
+    QPoint lastPoint;
+    // 背景图层
+    QImage backgroundImage;
+    // 绘画图层
+    QImage drawingImage;
+};
+
+#endif // DRAWINGWIDGET_H
---
+++ DrawingMini --ver 0.2/mainwindow.cpp
@@ -0,0 +1,90 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "drawingwidget.h"
+#include "drawingtools.h"
+#include <QPainter>
+#include <QStatusBar>
+#include <QButtonGroup>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QSlider>
+#include <QColorDialog>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setMouseTracking(true);
+    this->statusBar()->show();
+    this->setWindowTitle("绘图小程序");
+    setWindowState(Qt::WindowMaximized);
+
+    QWidget *centralWidget = new QWidget(this);
+    this->setCentralWidget(centralWidget);
+
+
+
+    //仅供调试用，实际界面中应进行修改
+    //********************
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+    DrawingWidget *drawwidget = new DrawingWidget(centralWidget);
+
+    //QPushButton用于选择工具
+    QButtonGroup *group = new QButtonGroup(this);
+    QPushButton *penButton = new QPushButton("pencil", centralWidget);
+    QPushButton *eraserButton = new QPushButton("eraser", centralWidget);
+    group->addButton(penButton);
+    group->addButton(eraserButton);
+    group->setExclusive(true);
+    // 连接按钮点击信号
+    connect(penButton, &QPushButton::clicked,penButton, [drawwidget]() {
+        drawwidget->pen->setMode(0);//pencil工具
+    });
+    connect(eraserButton, &QPushButton::clicked,eraserButton, [drawwidget]() {
+        drawwidget->pen->setMode(1);//eraser工具
+    });
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+
+    QString buttonStyleSheet = "QPushButton { color: black; background-color: white; }";
+    penButton->setStyleSheet(buttonStyleSheet);
+    eraserButton->setStyleSheet(buttonStyleSheet);
+
+    buttonLayout->addWidget(penButton);
+    buttonLayout->addWidget(eraserButton);
+
+    // 创建 QSlider 用于调整粗细
+    QSlider *sizeSlider = new QSlider(Qt::Horizontal, centralWidget);
+    sizeSlider->setRange(1, 20);
+    sizeSlider->setValue(3);
+
+    // 连接滑块值改变信号到槽函数
+    connect(sizeSlider, &QSlider::valueChanged,sizeSlider, [drawwidget](int value) {
+        drawwidget->pen->setSize(value);
+    });
+
+    //创建QPushButton用于颜色选择
+    QPushButton *colorChoose=new QPushButton("color",centralWidget);
+    colorChoose->setStyleSheet(buttonStyleSheet);
+    connect(colorChoose,&QPushButton::clicked,colorChoose,[drawwidget,this](){
+        QColor newColor=QColorDialog::getColor(Qt::white, this);
+        drawwidget->pen->setColor(newColor);
+    });
+
+    layout->addLayout(buttonLayout);
+    layout->addWidget(colorChoose);
+    layout->addWidget(sizeSlider);
+    layout->addWidget(drawwidget);
+
+    //********************
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
