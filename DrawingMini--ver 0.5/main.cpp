2058c65e366cd51c35f858e4140cff77c22eaa91
Zjhjunhao <2751026122@qq.com>
2025-05-25 12:15:06 +0800

    添加主图标

--- DrawingMini--ver 0.5/main.cpp
+++ DrawingMini--ver 0.5/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/mainwindow.jpg"));
     MainWindow w;
     w.show();
     return a.exec();
